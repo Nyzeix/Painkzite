0244806b920e70cdb5a1d1f13252db316d3b8825
Nyzeix <nicolas46.ns@gmail.com>
2026-06-29 15:07:39 +0200

    added hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+RELEASE="$(rpm -E %fedora)"
+
+
+cp -avf "/ctx/system_files"/. /
+
 set -ouex pipefail
 
-# Copy the contents of system_files/ of the git repo to /
-cp -avf "/ctx/system_files"/. /
+
+enable_copr() {
+    repo="$1"
+    repo_with_dash="${repo/\//-}"
+    wget "https://copr.fedorainfracloud.org/coprs/${repo}/repo/fedora-${RELEASE}/${repo_with_dash}-fedora-${RELEASE}.repo" \
+        -O "/etc/yum.repos.d/_copr_${repo_with_dash}.repo"
+}
 
 ### Install packages
 
@@ -14,6 +24,52 @@ cp -avf "/ctx/system_files"/. /
 
 # this installs a package from fedora repos
 dnf5 install -y tmux
+dnf5 remove -y fish
+
+dnf5 install -y --setopt=install_weak_deps=False \
+    xdg-desktop-portal-hyprland \
+    hyprland \
+    hyprlock \
+    hypridle \
+    hyprpicker \
+    hyprsysteminfo \
+    hyprsunset \
+    hyprpaper \
+    hyprcursor \
+    hyprgraphics \
+    hyprpolkitagent \
+    hyprland-qtutils \
+    hyprland-qt-support \
+    hyprland-uwsm \
+    uwsm \
+    pyprland \
+    waybar \
+    wofi \
+    rofi \
+    swaync \
+    wl-clipboard \
+    grim \
+    brightnessctl \
+    pavucontrol \
+    network-manager-applet \
+    clipman \
+    nwg-drawer \
+    wdisplays \
+    pavucontrol \
+    SwayNotificationCenter \
+    NetworkManager-tui \
+    tmux \
+    ghostty \
+    wezterm \
+    blueman \
+    qt5-qtwayland \
+    qt6-qtwayland \
+    sddm
+
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable solopasha/hyprland
+# dnf5 -y copr disable erikreider/SwayNotificationCenter
+# dnf5 -y copr disable pgdev/ghostty
 
 # Use a COPR Example:
 #
@@ -25,3 +81,7 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl disable gdm
+systemctl enable sddm
+
+mkdir -p /nix/var/nix/gcroots/per-user/bazzite
